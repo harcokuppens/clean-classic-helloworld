1ea01456863d8fa1e1c9804483b11217d54c97d6
Harco Kuppens <h.kuppens@gmail.com>
2025-01-18 09:45:45 +0100

    Update repocleanup.bash

--- repocleanup.bash
+++ repocleanup.bash
@@ -3,7 +3,9 @@ set -e
 
 script_dir=$(dirname $0)
 rm -rf "$script_dir/bin"
-rm -rf "$script_dir/src/Clean System Files"
+# cleanup builds in src folders in "Clean System Files" folders
+find "$script_dir/src"  -name 'Clean System Files' -type d -exec echo {} ';'  | xargs -I % rm -r "%"
+
 
 # also cleanup clean folder 
 rm -rf "$script_dir/clean"
